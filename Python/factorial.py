06f02554c0cdee8da4d4ff287139fd2275d4af5b
Shubham Menroy <54736939+shubham9672@users.noreply.github.com>
2020-10-14 22:35:34 +0530

    Create factorial.py
    Factorial using recursion in python

---
+++ Python/factorial.py
@@ -0,0 +1,13 @@
+def fact(n):
+	if n==1 :
+		return 1
+	else:
+	    return n*fact(n-1)
+n=int(input("Enter the number whoes factorial you want to find\n"))
+if n<0:
+	print("Not possible you have entered negative number")
+elif n==0:
+	print("0")
+else:
+    result=fact(n)
+    print(result)
