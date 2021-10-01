3fa853bc7a31ceb1bbb92ef3d077d5aa2eae1e54
Yashika Jain <61237800+Yashika1410@users.noreply.github.com>
2021-10-01 10:28:32 +0530

    Create CiaserCypher.py

---
+++ Python/CiaserCypher.py
@@ -0,0 +1,30 @@
+def caesar_encrypt(word,n):
+    c = ''
+    for i in word:
+        if (not i.isalpha()):
+            c += i
+        elif (i.isupper()):
+            c += chr((ord(i) + n-65) % 26 + 65)
+        else:
+            c += chr((ord(i) + n - 97) % 26 + 97)
+    return c
+
+def caesar_decrypt(word,n):
+    c = ''
+    for i in word:
+        if (not i.isalpha()):
+            c += i
+        elif (i.isupper()):
+            c += chr((ord(i) - n -65) % 26 + 65)
+ 
+        # Encrypt lowercase characters
+        else:
+            c += chr((ord(i) - n - 97) % 26 + 97)
+    return c
+  
+plain = input()
+n=int(input())
+cipher = caesar_encrypt(plain,n)
+print(cipher)
+decipher = caesar_decrypt(cipher,n)
+print(decipher)
