2bc457927db8b9be5c02a2e13e8e191db0455a0c
wilbert <wilberthy11@gmail.com>
2021-10-06 15:14:25 +0700

    reformat python file based on PEP 8 format & adding insertion sort algorithm python file

--- Python/CiaserCypher.py
+++ Python/CiaserCypher.py
@@ -1,30 +1,32 @@
-def caesar_encrypt(word,n):
+def caesar_encrypt(word, n):
     c = ''
     for i in word:
         if (not i.isalpha()):
             c += i
         elif (i.isupper()):
-            c += chr((ord(i) + n-65) % 26 + 65)
+            c += chr((ord(i) + n - 65) % 26 + 65)
         else:
             c += chr((ord(i) + n - 97) % 26 + 97)
     return c
 
-def caesar_decrypt(word,n):
+
+def caesar_decrypt(word, n):
     c = ''
     for i in word:
         if (not i.isalpha()):
             c += i
         elif (i.isupper()):
-            c += chr((ord(i) - n -65) % 26 + 65)
- 
+            c += chr((ord(i) - n - 65) % 26 + 65)
+
         # Encrypt lowercase characters
         else:
             c += chr((ord(i) - n - 97) % 26 + 97)
     return c
-  
+
+
 plain = input()
-n=int(input())
-cipher = caesar_encrypt(plain,n)
+n = int(input())
+cipher = caesar_encrypt(plain, n)
 print(cipher)
-decipher = caesar_decrypt(cipher,n)
+decipher = caesar_decrypt(cipher, n)
 print(decipher)
