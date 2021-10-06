1cae06fcd1966fb7cd4f95c7df0b6b8f3a73c666
Chirag2007 <72174667+Chirag2007@users.noreply.github.com>
2021-10-06 10:16:48 +0530

    Multiplication_Table.py

---
+++ Python/Multiplication Table.py
@@ -0,0 +1,19 @@
+# Hello Friends This is a code which shows the multiplation table of any number just run this code below, it will ask of the number you want to know the table of.
+# after then it will show the multiplication table of that following number
+# IF YOU WANT TO UPDATE THIS CODE FEEL FREE TO DO IT 
+# Thnkyou
+
+
+
+
+
+# Multiplication table (from 1 to 10) in Python
+
+num = 12
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
