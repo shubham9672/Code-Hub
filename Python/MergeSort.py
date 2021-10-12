098f38dc2c6ab9880fa7e1f31e90266da1731f48
Rahul Sahu <65137108+rahulsahurs@users.noreply.github.com>
2021-10-12 11:51:05 +0530

    Update MergeSort.py

--- Python/MergeSort.py
+++ Python/MergeSort.py
@@ -28,3 +28,38 @@ def merge(left_half,right_half):
 
 unsorted_list = [80, 44, 28, 100, 22, 15, 34]
 print(merge_sort(unsorted_list))
+
+#Merge Sort
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                nlist[k]=lefthalf[i]
+                i=i+1
+            else:
+                nlist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            nlist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            nlist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",nlist)
+
+nlist = [14,46,43,27,57,41,45,21,70]
+mergeSort(nlist)
+print(nlist)
