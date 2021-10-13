3f093b49a365d3b264db824cdc4ba84e52cdde68
Ujwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2021-10-13 13:17:50 +0530

    program is optimal
    add comment for easily understand code

--- JAVA/minimum_no_array.java
+++ JAVA/minimum_no_array.java
@@ -1,20 +1,16 @@
-import java.io.*;
-
-class proko{
-    
-
-    public static void main(String args[]){
-        int arr[]= {10,20,30,40,50,60};
-        int l= arr.length;
-        int min=0;
-
-        for(int i=0;i<l;i++){
-            if(arr[i]<min){
-                min=arr[i];
-                System.out.println(min);
-            }
-            
-            
-        }
-   }
-}
+public class smallestelement_array {  
+    public static void main(String[] args) {  
+  
+        //Initialize array  
+        int [] arr = new int [] {25, 11, 7, 75, 56};  
+        //Initialize min with first element of array.  
+        int min = arr[0];  
+        //Loop through the array  
+        for (int i = 0; i < arr.length; i++) {  
+            //Compare elements of array with min  
+           if(arr[i] <min)  
+               min = arr[i];  
+        }  
+        System.out.println("Smallest element present in given array: " + min);  
+    }  
+}  
