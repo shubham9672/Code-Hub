8ecf5bb727eeef488d04566b6173045a01972480
Reetu Raj <56961360+rajcodes03@users.noreply.github.com>
2021-10-15 12:58:01 +0530

    Create fake_info_generator.py

---
+++ Python/fake_info_generator.py
@@ -0,0 +1,7 @@
+from faker import Faker
+fake = Faker()
+print(fake.name())
+print(fake.email())
+print(fake.country())
+
+print(fake.profile())                                                        
