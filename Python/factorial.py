4466769388e9616cd7219e77a6c28b516015de94
Shyam Radadiya <67785558+shyam-ra18@users.noreply.github.com>
2021-10-20 14:43:19 +0530

    update programm
    easy way to find factorial using recursion. and 0&1 factorial is =1 updated

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,13 +1,10 @@
+num = input("Enter the number whoes factorial you want to find: ")
 def fact(n):
-	if n==1 :
-		return 1
-	else:
-	    return n*fact(n-1)
-n=int(input("Enter the number whoes factorial you want to find\n"))
-if n<0:
-	print("Not possible you have entered negative number")
-elif n==0:
-	print("0")
-else:
-    result=fact(n)
-    print(result)
+    if n == 1 or n == 0:
+       return 1
+    elif n < 0:
+       return ("NA")
+    else:
+       return n*fact(n-1)
+
+print (fact(int(num)))
