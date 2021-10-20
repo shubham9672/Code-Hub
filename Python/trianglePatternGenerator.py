0121c57919f1b4529ad31f7a1efa08f26e7b06b6
coderGtm <66418526+coderGtm@users.noreply.github.com>
2021-10-20 23:17:34 +0530

    Create trianglePatternGenerator.py

---
+++ Python/trianglePatternGenerator.py
@@ -0,0 +1,13 @@
+def triangle(n):
+    k = n - 1
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
+        
+#__main__
+n = 5
+triangle(n)
